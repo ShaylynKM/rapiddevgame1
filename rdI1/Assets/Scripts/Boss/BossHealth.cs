b21eea095b9f040d4b74d8b030e41bac0305e7af
yayuli <pengyufentf@outlook.com>
2024-02-26 23:33:27 -0700

    update
    Add a school background layer
    Cancel the health bar and damage of enemies in home, school, and work scenes

--- rdI1/Assets/Scripts/Boss/BossHealth.cs
+++ rdI1/Assets/Scripts/Boss/BossHealth.cs
@@ -7,59 +7,18 @@ using UnityEngine.UI;
 
 public class BossHealth : MonoBehaviour
 {
-    public int maxHealth = 100;
-    public int health;
-    private int currentHealth;
     private SpriteRenderer spriteRenderer; 
     public Color hurtColor = Color.red;
     private Color originalColor; 
     public float colorChangeDuration = 0.5f;
 
-    public GameObject winScreen;
-    public Slider healthBar; 
-
-
     void Start()
     {
-        currentHealth = maxHealth;
-
-        healthBar.maxValue = maxHealth;
-        healthBar.value = currentHealth;
-
         spriteRenderer = GetComponent<SpriteRenderer>(); // Get SpriteRenderer component
         if (spriteRenderer != null)
         {
             originalColor = spriteRenderer.color; // Save original color
         }
-    }
-
-    public void TakeDamage(int damage)
-    {
-        currentHealth -= damage;
-        
-        Debug.Log("Boss current health is" + currentHealth);
-
-        //Make sure the health value never drops below 0
-        currentHealth = Mathf.Max(currentHealth, 0);
-
-        // Update health bar
-        healthBar.value = currentHealth;
-
-        // Update boss health UI here
-
-        if (currentHealth <= 0)
-        {
-            Defeated();
-            Debug.Log("Player win! ");
-        }
-    }
-
-   
-
-    void Win()
-    {
-        winScreen.SetActive(true);
-        Time.timeScale = 0f; 
     }
 
     public void TriggerColorChange()
@@ -74,18 +33,5 @@ public class BossHealth : MonoBehaviour
         yield return new WaitForSeconds(colorChangeDuration); // wait for seconds
         spriteRenderer.color = originalColor; // back original color
     }
-
-    private void Defeated()
-    {
-        Debug.Log("Boss Defeated");
-        AudioManager.Instance.Play(4, "bossKill", false);
-        Destroy(gameObject);
-        ShowWinScreen();
-    }
-
-    private void ShowWinScreen()
-    {
-        winScreen.SetActive(true);
-        Time.timeScale = 0f;
-    }
+    
 }
