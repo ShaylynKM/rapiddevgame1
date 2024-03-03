a2fcc5ec3d62741099370635bdc5b03a37c9d925
yayuli <pengyufentf@outlook.com>
2024-03-03 02:38:03 -0700

    Update
    Party's three-stage  ScriptableObject works

--- rdI1/Assets/ScriptableObjects/PartySettings/GamePhaseSO.cs
+++ rdI1/Assets/ScriptableObjects/PartySettings/GamePhaseSO.cs
@@ -6,11 +6,13 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "GamePhase", menuName = "GameFlow/GamePhase")]
 public class GamePhaseSO : ScriptableObject
 {
-    public string phaseName;
-    public DialogueSceneScriptableObject dialogue;
+    public float phaseDuration;
     public bool canMove;
     public bool canShoot;
     public bool canFreeze;
+    public GameObject anxietyMeterPrefab;
+    public GamePhaseSO nextPhase;
+    public DialogueSceneScriptableObject dialogueScene;
 
 }
 
